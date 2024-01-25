8d57f8f7f2203de790a7a79738404c36317deb07
bottlewook <bwlimtony@gmail.com>
2024-01-25 23:30:16 +0900

    feat: Curtain 컴포넌트 제작

---
+++ src/components/curtain/Curtain.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import classNames from 'classnames/bind';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import styles from './Curtain.module.scss';
+
+const cx = classNames.bind(styles);
+
+const curtainVariant = {
+  enter: {
+    transition: {
+      staggerChildren: 0.08,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const colorVariant = {
+  start: { scaleY: 1 },
+  enter: {
+    scaleY: 0,
+    transition: {
+      duration: 0.8,
+      ease: 'easeInOut',
+    },
+  },
+  end: {
+    scaleY: 1,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+function Curtain() {
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial="start"
+        animate="enter"
+        exit="end"
+        variants={curtainVariant}
+        className={cx('curtain')}
+      >
+        <motion.div variants={colorVariant} className={cx('item')} />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+export default Curtain;
