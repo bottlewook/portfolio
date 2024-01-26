9ecfe8c0f7bcf45ead7a381265dbe3ae54fe7593
bottlewook <bwlimtony@gmail.com>
2024-01-26 11:44:34 +0900

    feat: 메인 페이지 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,54 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import React, { useState } from 'react';
+
+import classNames from 'classnames/bind';
+import { motion } from 'framer-motion';
+
+import Curtain from '@components/curtain/Curtain';
+import Counter from '@components/init/Counter';
+import Intro from '@components/intro/Intro';
+import MainNav from '@components/main-nav/MainNav';
+import Project from '@components/project/Project';
+import useMoveScroll from '@hooks/useMoveScroll';
+
+import styles from './page.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default function Home() {
+  const [step, setStep] = useState(0);
+
+  const about = useMoveScroll();
+  const education = useMoveScroll();
+  const project = useMoveScroll();
+  const contact = useMoveScroll();
+
+  const sections = [about, education, project, contact];
+
+  const onNext = () => {
+    setStep((prev) => { return prev + 1; });
+  };
+
   return (
-    <main className={styles.main}>
-      <header>메인페이지</header>
-      <header>메인페이지</header>
-      <header>메인페이지</header>
-    </main>
-  )
+    <>
+      {step === 0 && (
+        <motion.div>
+          <Counter from={0} to={100} onNext={onNext} />
+        </motion.div>
+      )}
+      {step === 1 && (
+        <>
+          <Curtain />
+          <motion.div className={cx('container')}>
+            <MainNav sections={sections} />
+            <Intro ref={about.element} />
+            <Intro ref={education.element} />
+            <Project ref={project.element} />
+            <Intro ref={contact.element} />
+          </motion.div>
+        </>
+      )}
+    </>
+  );
 }
