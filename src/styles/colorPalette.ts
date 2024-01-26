6bcf97f858465d86049cabb7248a82bc7ee1f391
bottlewook <bwlimtony@gmail.com>
2024-01-26 11:42:37 +0900

    design: reset css 및 컬러팔레트 추가

---
+++ src/styles/colorPalette.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  primary: 'var(--primary)',
+  secondary: 'var(--secondary)',
+  red: 'var(--red)',
+  gray: 'var(--gray)',
+  gray100: 'var(--gray-100)',
+  gray200: 'var(--gray-200) ',
+  gray300: 'var(--gray-300)',
+  gray400: 'var(--gray-400)',
+  tertiary: 'var(--tertiary)',
+  tertiary100: 'var(--tertiary-100)',
+  tertiary200: 'var(--tertiary-200)',
+  tertiary300: 'var(--tertiary-300)',
+  tertiary400: 'var(--tertiary-400)',
+  black: 'var(--black)',
+  white: 'var(--white)',
+  white100: 'var(--white-100)',
+  pink: 'var(--pink)',
+};
+
+export type Colors = keyof typeof colors;
