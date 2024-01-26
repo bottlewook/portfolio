272474a6fe8b67f960ac9ffbe732617ceca81cfd
bottlewook <bwlimtony@gmail.com>
2024-01-27 05:43:50 +0900

    chore: Education 컴포넌트 문구 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import classNames from 'classnames/bind';
 import { motion } from 'framer-motion';
 
 import Curtain from '@components/curtain/Curtain';
+import Education from '@components/education/Education';
 import Counter from '@components/init/Counter';
 import Intro from '@components/intro/Intro';
 import MainNav from '@components/main-nav/MainNav';
@@ -15,6 +16,20 @@ import useMoveScroll from '@hooks/useMoveScroll';
 import styles from './page.module.scss';
 
 const cx = classNames.bind(styles);
+
+const educationParagraph = `
+패스트 캠퍼스 Kernel 360 백엔드 파이널 프로젝트 참여 경험 (2023.12 ~ 2024.02)
+- 웹앱 개발 경험
+- vitest와 storybook을 이용한 테스트 주도 개발
+- aws 배포 경험
+- 유저 친화적인 최적화 경험
+
+코드잇 부트 캠프 수료 (2023.03 ~ 2023.09)
+- 6개월의 과정 동안 프론트엔드 주요 기술 학습
+- 기획부터 MVP 개발까지 현직 백엔드 개발자와 디자이너와 소통하여 기획/개발/배포 경험
+- 뮤지션 저작권 정산, 매칭 플랫폼 그리고 가상화폐 거래소 주제에 대한 3가지 팀 프로젝트 경험
+- 적극적인 코드 리뷰와 동료와의 페어 프로그래밍 등 여러 협력 경험
+`;
 
 export default function Home() {
   const [step, setStep] = useState(0);
@@ -40,13 +55,13 @@ export default function Home() {
       {step === 1 && (
         <>
           <Curtain />
-          <motion.div className={cx('container')}>
+          <div className={cx('container')}>
             <MainNav sections={sections} />
             <Intro ref={about.element} />
-            <Intro ref={education.element} />
+            <Education ref={education.element} paragraph={educationParagraph} />
             <Project ref={project.element} />
             <Intro ref={contact.element} />
-          </motion.div>
+          </div>
         </>
       )}
     </>
