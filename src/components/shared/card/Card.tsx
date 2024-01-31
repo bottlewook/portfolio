d412047df372d9efdc319c5a617e0e896b74de4f
bottlewook <bwlimtony@gmail.com>
2024-01-31 20:31:40 +0900

    feat: Card 컴포넌트 이미지 사이즈 수정

--- src/components/shared/card/Card.tsx
+++ src/components/shared/card/Card.tsx
@@ -6,12 +6,12 @@ import { useRef } from 'react';
 
 import classNames from 'classnames/bind';
 import {
-  useTransform, useScroll, motion, MotionValue,
+  useTransform, motion, MotionValue,
 } from 'framer-motion';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-import Spacing from '../spacing/Spacing';
+import Spacing from '@shared/spacing/Spacing';
 
 import styles from './Card.module.scss';
 
@@ -28,19 +28,14 @@ interface CardProps {
   progress: MotionValue<number>
   range: number[]
   targetScale: number
+  type: string
 }
 
 function Card({
-  i, title, description, feature, src, link, color, progress, range, targetScale,
+  i, title, description, feature, src, link, color, progress, range, targetScale, type,
 }: CardProps) {
   const route = useRouter();
   const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ['start end', 'start start'],
-  });
-
-  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
 
   const scale = useTransform(progress, range, [1, targetScale]);
 
@@ -75,16 +70,12 @@ function Card({
             </ul>
           </div>
           <div className={styles.imageContainer}>
-            <motion.div
-              className={styles.inner}
-              style={{ scale: imageScale }}
-            >
-              <Image
-                fill
-                src={src}
-                alt="image"
-              />
-            </motion.div>
+            <Image
+              width={type === 'desktop' ? 530 : 375}
+              height={type === 'desktop' ? 330 : 567}
+              src={src}
+              alt="image"
+            />
           </div>
         </div>
       </motion.div>
@@ -97,8 +88,7 @@ export default Card;
 function ShareIcon({ onClick }: { onClick: () => void }) {
   return (
     <svg data-name="Layer 1" height="60" id="Layer_1" viewBox="0 0 200 200" width="60" xmlns="http://www.w3.org/2000/svg" onClick={onClick} style={{ cursor: 'pointer' }}>
-      <title />
-      <path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165ZM97.5,57.5a9.9,9.9,0,0,0-14,14L112,100,83.5,128.5a9.9,9.9,0,0,0,14,14L126,114a19.92,19.92,0,0,0,0-28.5Z" />
+      <path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165ZM97.5,57.5a9.9,9.9,0,0,0-14,14L112,100,83.5,128.5a9.9,9.9,0,0,0,14,14L126,114a19.92,19.92,0,0,0,0-28.5Z" fill="white" />
     </svg>
   );
 }
