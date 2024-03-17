511a0e769b0e7ff63f67bf7be54c25b9787b5f60
bottlewook <bwlimtony@gmail.com>
2024-03-17 14:32:49 +0900

    feat: 홈페이지 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,39 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 
 import classNames from 'classnames/bind';
-import { motion } from 'framer-motion';
 
-import AboutMe from '@components/about-me/AboutMe';
-import Contact from '@components/contact/Contact';
-import Curtain from '@components/curtain/Curtain';
+import AboutMe from '@components/about-me/AboutMe3';
 import Education from '@components/education/Education';
 import Footer from '@components/footer/Footer';
-import Counter from '@components/init/Counter';
-import Intro from '@components/intro/Intro';
 import MainNav from '@components/main-nav/MainNav';
 import Project from '@components/project/Project';
+import Skills from '@components/skills/Skills';
 import useMoveScroll from '@hooks/useMoveScroll';
-import Spacing from '@shared/spacing/Spacing';
 
 import styles from './page.module.scss';
 
 const cx = classNames.bind(styles);
 
 export default function Home() {
-  const [step, setStep] = useState(0);
-
-  const intro = useMoveScroll();
   const aboutMe = useMoveScroll();
-  const education = useMoveScroll();
   const project = useMoveScroll();
-  const contact = useMoveScroll();
+  const skills = useMoveScroll();
+  const education = useMoveScroll();
 
-  const sections = [intro, aboutMe, education, project, contact];
-
-  const onNext = () => {
-    setStep((prev) => { return prev + 1; });
-  };
+  const sections = [aboutMe, project, skills, education];
 
   return (
     <>
-      {step === 0 && (
-        <motion.div>
-          <Counter from={0} to={100} onNext={onNext} />
-        </motion.div>
-      )}
-      {step === 1 && (
-        <>
-          <Curtain />
-          <div className={cx('container')}>
-            <MainNav sections={sections} />
-            <Intro ref={intro.element} moveAboutMe={aboutMe.onMoveToElement} />
-            <AboutMe ref={aboutMe.element} />
-            <Education ref={education.element} />
-            <Spacing size={200} />
-            <Project ref={project.element} />
-            <Contact ref={contact.element} />
-            <Spacing size={200} />
-            <Footer />
-          </div>
-        </>
-      )}
+      <MainNav sections={sections} />
+      <div className={cx('container')}>
+        <AboutMe ref={aboutMe.element} />
+        <Project ref={project.element} />
+        <Skills ref={skills.element} />
+        <Education ref={education.element} />
+        <Footer />
+      </div>
     </>
   );
 }
